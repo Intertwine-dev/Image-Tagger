5f05262625d38e83729bdd86514181dc2a4818a1
semaxa <salvatore.imparato@intertwine.it>
2015-02-25 16:58:00 +0100

    Intial Commit

--- config/config.php
+++ config/config.php
@@ -1,17 +1,10 @@
 <?php
 
-define("ApiKeyAlchemy", "eb531806f5834b0b77cb2f1b4fcd2a6c00a20f1c");
 
-define("ApiKeyImagga", "acc_3888d9b709a4e05");
-define("ApiSecretImagga", "8d4abfe3f8dccaaedf9cb3eb19d12d34");
+define("ApiKeyAlchemy", "YOUR_API_KEY");
 
-define("ApiKeyRekognition", "LdhyumSnMRP6tb1A");
-define("ApiSecretRekognition", "khi3cggALxjqSE1H");
+define("ApiKeyImagga", "YOUR_API_KEY");
+define("ApiSecretImagga", "YOUR_API_SECRET");
 
-//define("ApiKeyAlchemy", "YOUR_API_KEY");
-//
-//define("ApiKeyImagga", "YOUR_API_KEY");
-//define("ApiSecretImagga", "YOUR_API_SECRET");
-//
-//define("ApiKeyRekognition", "YOUR_API_KEY");
-//define("ApiSecretRekognition", "YOUR_API_SECRET");
+define("ApiKeyRekognition", "YOUR_API_KEY");
+define("ApiSecretRekognition", "YOUR_API_SECRET");
